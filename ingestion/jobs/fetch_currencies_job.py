7df6ab4181b1fb1e250f0bbf4cf45cd2e4e1d254
Tanel Sitska <TanelS@users.noreply.github.com>
2026-03-18 20:09:53 +0200

    Refactor `get_currencies` to fetch and return response field, and replace testing call with printed output

--- ingestion/jobs/fetch_currencies_job.py
+++ ingestion/jobs/fetch_currencies_job.py
@@ -7,16 +7,15 @@ logger = logging.getLogger("Currency_symbols_fetch")
 
 def get_currencies() -> dict | None:
     data = fetch_from_api("currencies")
-    print(data)
     if not data:
         return None
 
     try:
-        return data
+        return data.get('response')
     except Exception as e:
         logger.exception(f"Failed to validate currencies response: {e}")
         return None
 
 
 if __name__ == "__main__":
-    get_currencies()   # for testing
+    print(get_currencies())  # for testing
