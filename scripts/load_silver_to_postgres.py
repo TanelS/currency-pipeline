c0984e46f29c4611848ab25b4d60220c94eff9cc
Tanel Sitska <TanelS@users.noreply.github.com>
2026-03-26 11:24:04 +0200

    Drop and recreate constraints for staging tables, add logging, and improve docstrings in `load_silver_to_postgres.py`.

--- scripts/load_silver_to_postgres.py
+++ scripts/load_silver_to_postgres.py
@@ -1,7 +1,7 @@
 import os
 
 import psycopg
-
+import logging
 from db import JDBC_URL, conn_string, jdbc_props
 from spark.config.spark_config import SILVER_DIR
 from spark.session.builder import get_spark
@@ -9,17 +9,19 @@ from spark.session.builder import get_spark
 silver_path_currencies = os.path.join(SILVER_DIR, 'currencies')
 silver_path_rates = os.path.join(SILVER_DIR, 'rates')
 
+logger = logging.getLogger("Currencies-rates-to-db-staging")
 
 def load_currencies_to_stage(spark):
     """
-    Loads currency data from a Delta table into a stage table in a PostgreSQL database.
+    Loads currency data from a Delta table into a staging table in the database.
 
-    This function reads data from a Silver Delta table containing currency information,
-    displays the row count and table preview, and then writes the data into a stage table.
-    Finally, it adds a primary key constraint to the stage table.
+    This function reads data related to currencies from a specified Delta table
+    and writes it to a staging table in the database. The function manages
+    foreign key constraints and primary keys during the process to ensure
+    seamless data loading.
 
-    :param spark: A SparkSession object used to interact with Spark.
-    :type spark: pyspark.sql.session.SparkSession
+    :param spark: The Spark session used to read data from the Delta table.
+    :type spark: pyspark.sql.SparkSession
     :return: None
     """
     print(f'Reading Silver currencies from: {silver_path_currencies}')
@@ -28,25 +30,38 @@ def load_currencies_to_stage(spark):
     df.show()
 
     print("Writing currencies to stage table...")
+
+    # Drop FKs BEFORE jdbc write
+    try:
+        with psycopg.connect(conn_string) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute('ALTER TABLE public.rates_stage DROP CONSTRAINT IF EXISTS base_fk')
+                cursor.execute('ALTER TABLE public.rates_stage DROP CONSTRAINT IF EXISTS curr_fk')
+            conn.commit()
+    except Exception as e:
+        logger.exception(f"Failed to drop foreign key constraints: {e}")
+
     writer = df.write.mode('overwrite').option('truncate', 'true')
     writer.jdbc(JDBC_URL, 'public.currencies_stage', properties=jdbc_props)
     print('===  Currencies stage table loaded.')
 
     with psycopg.connect(conn_string) as conn:
+
         with conn.cursor() as cursor:
+            cursor.execute('ALTER TABLE public.currencies_stage DROP CONSTRAINT IF EXISTS currencies_stage_pkey')
             cursor.execute('ALTER TABLE public.currencies_stage ADD PRIMARY KEY (short_code)')
         conn.commit()
 
 
 def load_rates_to_stage(spark):
     """
-    Load exchange rates data from a Delta table into a staging table in a PostgreSQL database.
+    Loads exchange rates data into a staging table and applies necessary constraints.
 
-    This function reads data from the Delta table specified by the path `silver_path_rates`,
-    writes it to a staging table (`public.rates_stage`) in the database while overwriting
-    existing content, and sets up primary and foreign key constraints on the staging table.
+    The function reads data from a Delta table containing exchange rates, writes it to a
+    staging table in a PostgreSQL database, and configures primary and foreign key constraints
+    on the staging table.
 
-    :param spark: Spark session used for reading from the Delta table and processing data.
+    :param spark: A SparkSession instance used to interact with Spark.
     :type spark: pyspark.sql.SparkSession
 
     :return: None
@@ -65,10 +80,12 @@ def load_rates_to_stage(spark):
 
     with psycopg.connect(conn_string) as conn:
         with conn.cursor() as cursor:
+            cursor.execute('ALTER TABLE public.rates_stage DROP CONSTRAINT IF EXISTS base_fk')
+            cursor.execute('ALTER TABLE public.rates_stage DROP CONSTRAINT IF EXISTS curr_fk')
+            cursor.execute('ALTER TABLE public.rates_stage DROP CONSTRAINT IF EXISTS rates_stage_pkey')
             cursor.execute('ALTER TABLE public.rates_stage ADD PRIMARY KEY (curr_base, currency, rate_date)')
             cursor.execute('ALTER TABLE public.rates_stage ADD CONSTRAINT base_fk FOREIGN KEY (curr_base) REFERENCES public.currencies_stage (short_code)')
             cursor.execute('ALTER TABLE public.rates_stage ADD CONSTRAINT curr_fk FOREIGN KEY (currency) REFERENCES public.currencies_stage (short_code)')
-
         conn.commit()
 
 
